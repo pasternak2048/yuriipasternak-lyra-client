0141e234f014555c63300a0153c3655b64250700
pasternak2048 <10pka.muscatel@gmail.com>
2025-06-11 18:56:20 +0300

    [LYRA.Client]: moved headers names into the static class LyraHeaderNames

---
+++ src/LYRA.Client/Constants/LyraHeaderNames.cs
@@ -0,0 +1,18 @@
+﻿namespace LYRA.Client.Constants
+{
+    /// <summary>
+    /// Contains the standard LYRA HTTP header names used for signed requests.
+    /// </summary>
+    public static class LyraHeaderNames
+    {
+        public const string Caller = "x-lyra-caller";
+        public const string Target = "x-lyra-target";
+        public const string Method = "x-lyra-method";
+        public const string Path = "x-lyra-path";
+        public const string Payload = "x-lyra-payload";
+        public const string PayloadHash = "x-lyra-payload-hash";
+        public const string Timestamp = "x-lyra-timestamp";
+        public const string Context = "x-lyra-context";
+        public const string Signature = "x-lyra-signature";
+    }
+}
